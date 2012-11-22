c4daee3a84b7f9733ffc0d438ee1dba55e3066e2
acolin <acolin@incaztech.com>
2012-11-22 12:16:20 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yanapana::Application.config.secret_token = 'c2357ab42008dc6c6704b0106a0c880aa403385a0dee02778b4b1438880b46e50e4a9f8e9259225a96aff7ed34a4a4729dea018110db947d2e864c55fe8cf3ff'
