29308c5e52e6d8d8073f94fb7a3766207cd5eb28
rlara <rlara@incaztech.com>
2013-01-15 11:09:30 -0600

    ya no jala el detalle del producto

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -4,5 +4,15 @@ Spree::Product.class_eval do
   belongs_to :cause
 
   validate :cause_id, :presence => true
+
+  before_save :save_date
+
+  private
+
+  def save_date
+  self.available_on = self.cause.date_start
+
+  end
+
 end
 
