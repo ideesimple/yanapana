e5158cf44396101ae75754fdee94307df8001d10
Gilberto Tovar <gtovar@incaztech.com>
2013-01-31 12:33:14 -0600

    --amed

---
+++ app/overrides/loginform.rb
@@ -0,0 +1,6 @@
+Deface::Override.new(:virtual_path => "spree/checkout/resigstration",
+                     :insert_before => "div#password-credentials",
+                     :text => "<p>Prueba 1010101010 </p>",
+                     :name => "nose que es name"
+                    )
+
