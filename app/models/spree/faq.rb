c3ea7ba471a19d84b852beda041c03546384202d
rlara <rlara@incaztech.com>
2013-05-03 14:25:14 -0500

    faqs order jquery

--- app/models/spree/faq.rb
+++ app/models/spree/faq.rb
@@ -1,4 +1,6 @@
 class Spree::Faq < ActiveRecord::Base
   attr_accessible :answer, :question, :position
   acts_as_list
+
+
 end
--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -11,4 +11,10 @@ Spree::Order.class_eval do
     variantes = variantes.flatten
     Spree::LineItem.where('order_id in (?) AND variant_id in (?)', Spree::Order.complete.map(&:id), variantes).select('distinct order_id')
   end
+
+  def self.total_items(variantes)
+    variantes = variantes.flatten
+    Spree::LineItem.where('order_id in (?) AND variant_id in (?)', Spree::Order.complete.map(&:id), variantes).sum(&:quantity)
+  end
+
 end
