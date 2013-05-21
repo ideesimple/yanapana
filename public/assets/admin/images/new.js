f7ea8b401b3792dc4c4ab1536f2017749b57b735
rlara <rlara@incaztech.com>
2013-05-21 16:33:38 -0500

    delete public assets

--- public/assets/admin/images/new.js
+++
@@ -1,9 +0,0 @@
-(function() {
-
-  ($('#cancel_link')).click(function(event) {
-    event.preventDefault();
-    ($('#new_image_link')).show();
-    return ($('#images')).html('');
-  });
-
-}).call(this);
